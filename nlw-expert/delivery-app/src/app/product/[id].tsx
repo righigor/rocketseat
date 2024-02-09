54eafbbfcf87b6ae35106334a80eb8b63d25fa73
Igor Righi <cruigor@gmail.com>
2024-02-09 14:39:53 -0300

    finalizando aula 03

--- nlw-expert/delivery-app/src/app/product/[id].tsx
+++ nlw-expert/delivery-app/src/app/product/[id].tsx
@@ -1,5 +1,5 @@
 import { Image, Text, View } from "react-native";
-import { useLocalSearchParams, useNavigation } from "expo-router";
+import { useLocalSearchParams, useNavigation, Redirect } from "expo-router";
 import { Feather } from "@expo/vector-icons";
 
 import { useCartStore } from "@/stores/cart-store";
@@ -15,13 +15,16 @@ export default function Product() {
   const navigation = useNavigation();
   const { id } = useLocalSearchParams();
 
-  const product = PRODUCTS.filter((item) => item.id === id)[0];
+  const product = PRODUCTS.find((item) => item.id === id);
 
   function handleAddToCart() {
-    cartStore.add(product);
+    cartStore.add(product!);
     navigation.goBack();
   }
 
+  if(!product) {
+    return <Redirect href="/" />;
+  }
 
   return (
     <View className="flex-1">
@@ -31,9 +34,13 @@ export default function Product() {
         resizeMode="cover"
       />
 
+
       <View
         className="p-5 mt-8 flex-1"
       >
+        <Text className="text-white text-xl font-heading">
+          {product.title}
+        </Text>
         <Text className="text-lime-400 text-2xl font-heading">
           {formatCurrency(product.price)}
         </Text>
--- nlw-expert/delivery-app/src/stores/cart-store.ts
+++ nlw-expert/delivery-app/src/stores/cart-store.ts
@@ -1,6 +1,10 @@
-import { ProductProps } from "@/utils/data/products";
 import { create } from "zustand";
+import { createJSONStorage, persist } from 'zustand/middleware';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 import * as cartInMemory from "./helpers/cart-in-memory";
+
+import { ProductProps } from "@/utils/data/products";
 
 export type ProductCartProps = ProductProps & {
   quantity: number,
@@ -9,11 +13,21 @@ export type ProductCartProps = ProductProps & {
 type StateProps = {
   products: ProductCartProps[],
   add: (product: ProductProps) => void,
+  remove: (productId: string) => void,
+  clear: () => void,
 }
 
-export const useCartStore = create<StateProps>((set) => ({
+export const useCartStore = create(
+  persist<StateProps>((set) => ({
   products: [],
   add: (product: ProductProps) => set((state) => ({
     products: cartInMemory.add(state.products, product),
   })),
+  remove: (productId: string) => set((state) => ({
+    products: cartInMemory.remove(state.products, productId),
+  })),
+  clear: () => set(() => ({ products: [] })),
+}), {
+  name: "delivery-app:cart",
+  storage: createJSONStorage(() => AsyncStorage),
 }))
